0562f240e38a9d54ab6c543590c4357c9fcb3cbd
Pyter Pantoufle <pyd3.14@gmail.com>
2020-07-28 16:41:54 +0200

    Add security to form (but unnoptimal)

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -4,7 +4,6 @@ var app = {
 
 		console.log('app init');
 
-
 		/**
 		 * *****************************
 		 * L I S T E N E R S
@@ -51,22 +50,25 @@ var app = {
 	 */
 	openFiltersModal: function () {
 		console.log('open modal filters');
-		var modal = $('.filters-container');
+		let modal = $('.filters-container');
 		modal.css("height", "100%")
 	},
 
 	closeFiltersModal: function () {
-		var closeModal = $('.filters-container');
+		let closeModal = $('.filters-container');
 		closeModal.css("height", "0%")
 	},
 
 	filtersRecipies: function (e) {
 		e.preventDefault();
 
-		var recipiesData = $('.recipe .filter-data-recipe');
-		var filters = $('.filters-menus input[type=checkbox]');
+		const recipiesData = $('.recipe .filter-data-recipe');
+		const filters = $('.filters-menus input[type=checkbox]');
+		const filtersTags = $('.filters-tags input[type=checkbox]');
 
-		var filtersArray = [];
+		console.log(filtersTags);
+
+		const filtersArray = [];
 		for (filter of filters) {
 			if (filter.checked) {
 				filtersArray.push(filter.value);
@@ -75,13 +77,21 @@ var app = {
 
 		for (recipeData of recipiesData) {
 
-			var menu = recipeData.getAttribute('data-menu');
+			let menu = recipeData.getAttribute('data-menu');
 			if (!filtersArray.includes(menu)) {
 				recipeDiv = recipeData.parentNode.parentNode.parentNode.parentNode;
-				recipeDiv.remove();
+				recipeDiv.classList.remove('recipe');
+				recipeDiv.classList.add('hide');
 				console.log(menu + ' removed');
 			}
+
+			if (!filters.checked) {
+				recipeDiv.classList.remove('hide');
+				recipeDiv.classList.add('recipe');
+			}
 		}
+
+		app.closeFiltersModal();
 	},
 
 	/**
@@ -132,22 +142,22 @@ var app = {
 		console.log('Recherche de recettes');
 
 		// Disable submit form on Enter keypress
-		var keyCode = e.keyCode || e.which;
+		const keyCode = e.keyCode || e.which;
 		if (keyCode === 13) {
 			e.preventDefault();
 
 			// Add new input
-			var addInput = $('.add-ingredient-link');
+			let addInput = $('.add-ingredient-link');
 			addInput.trigger("click");
 
 			// Get next input
-			var i = $('.search-by-ingredient-input:last').attr('name');
-			var getInteger = i.match(/(\d+)/);
+			let i = $('.search-by-ingredient-input:last').attr('name');
+			let getInteger = i.match(/(\d+)/);
 			i = parseInt(getInteger);
 
 			// Focus to next input
 			setTimeout(function () {
-				var pouet = $('#search_recipe_ingredient_' + i + '_name');
+				let pouet = $('#search_recipe_ingredient_' + i + '_name');
 				pouet.focus();
 			}, 30);
 
@@ -158,4 +168,3 @@ var app = {
 
 // App Loading
 document.addEventListener('DOMContentLoaded', app.init);
-
