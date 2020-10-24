4527a8d854c06a8891346c1f55f92734281fcd8d
py <pyd3.14@gmail.com>
2020-10-24 11:01:19 +0200

    Add fixtures for units

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Unit;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
@@ -9,9 +10,64 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        /*         $sel = new Ingredient();
-                $sel->setName('sel');
-                $manager->persist($sel);
-                $manager->flush(); */
+        $unit = new Unit;
+        $unit->setName('unité');
+        $manager->persist($unit);
+        $manager->flush();
+
+        $mg = new Unit;
+        $mg->setName('mg');
+        $manager->persist($mg);
+        $manager->flush();
+
+        $g = new Unit;
+        $g->setName('g');
+        $manager->persist($g);
+        $manager->flush();
+
+        $kg = new Unit;
+        $kg->setName('kg');
+        $manager->persist($kg);
+        $manager->flush();
+
+        $ml = new Unit;
+        $ml->setName('ml');
+        $manager->persist($ml);
+        $manager->flush();
+
+        $dl = new Unit;
+        $dl->setName('dl');
+        $manager->persist($dl);
+        $manager->flush();
+
+        $cl = new Unit;
+        $cl->setName('cl');
+        $manager->persist($cl);
+        $manager->flush();
+
+        $l = new Unit;
+        $l->setName('Litre');
+        $manager->persist($l);
+        $manager->flush();
+
+        $pincee = new Unit;
+        $pincee->setName('pincée');
+        $manager->persist($pincee);
+        $manager->flush();
+
+        $cf = new Unit;
+        $cf->setName('cuillère à café');
+        $manager->persist($cf);
+        $manager->flush();
+
+        $cs = new Unit;
+        $cs->setName('cuillère à soupe');
+        $manager->persist($cs);
+        $manager->flush();
+
+        $tasse = new Unit;
+        $tasse->setName('tasse');
+        $manager->persist($tasse);
+        $manager->flush();        
     }
 }
